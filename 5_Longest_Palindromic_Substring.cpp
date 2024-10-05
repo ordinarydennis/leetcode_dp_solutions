ca64fac34a044b4dbea92aacee97c0b2f32abbfa
ordinarydennis <ordinarydennis@gmail.com>
2024-10-05 19:16:12 +0900

    DP - 5. Longest Palindromic Substring(read editorial)

--- 5_Longest_Palindromic_Substring.cpp
+++ 5_Longest_Palindromic_Substring.cpp
@@ -55,28 +55,22 @@ class Solution {
 public:
 	string longestPalindrome(string s) {
 
-		vector<vector<int>> dp(s.size(), vector<int>(s.size()));
+		vector<vector<bool>> dp(s.size(), vector<bool>(s.size()));
 
-
-		string ret;
-
-		int max = INT_MIN;
+		pair<int, int> pos = { 0, 0 };
 
 		for (int i = 0; i < s.size(); i++)
 		{
-			dp[i][i] = 1;
-
-			ret = s.substr(i, 1);
-			
-			max = 1;
+			dp[i][i] = true;
 
 			if (i < s.size() - 1)
 			{
 				if (s[i] == s[i + 1])
 				{
-					dp[i][i + 1] = 2;
-					ret = s.substr(i, 2);
-					max = 2;
+					dp[i][i + 1] = true;
+
+					pos.first = i;
+					pos.second = i + 1;
 				}
 			}
 		}
@@ -88,18 +82,136 @@ public:
 			{
 				if (s[si] == s[ei] && s[si + 1] == s[ei - 1])
 				{
-					dp[si][ei] = dp[si + 1][ei - 1] + 1;
+					dp[si][ei] = true;
 
-					if (max < ei - si + 1)
+					if (pos.second - pos.first < ei - si)
 					{
-						max = ei - si + 1;
-						ret = s.substr(si, ei - si + 1);
+						pos.first = si;
+						pos.second = ei;
 					}
 				}
 				
 			}
 		}
 
+		return s.substr(pos.first, pos.second - pos.first + 1);
+	}
+};
+
+
+class Solution {
+public:
+	string longestPalindrome(string s) {
+
+		vector<vector<int>> dp(s.size(), vector<int>(s.size()));
+
+		int index = 0;
+		int length = 1;
+
+		for (int i = 0; i < s.size(); i++)
+		{
+			dp[i][i] = true;
+
+			if (i + 1 < s.size() && s[i] == s[i + 1])
+			{
+				dp[i][i + 1] = true;
+				index = i;
+				length = 2;
+			}
+		}
+
+		for (int l = 3; l <= s.size(); l++)
+		{
+			for (int i = 0; i <= s.size() - l; i++)
+			{
+				int e = i + l - 1;
+				if (s[i] == s[e] && dp[i + 1][e - 1])
+				{
+					dp[i][e] = true;
+					index = i;
+					length = l;
+				}
+			}
+		}
+
+		return s.substr(index, length);
+	}
+};
+
+class Solution {
+
+	bool isPalindrome(string& s)
+	{
+		int left = 0;
+		int right = s.size() - 1;
+
+		while (left < right)
+		{
+			if (s[left] != s[right])
+			{
+				return false;
+			}
+
+			left++;
+			right--;
+		}
+
+		return true;
+	}
+
+
+public:
+	string longestPalindrome(string s) {
+
+		string ret;
+		for (int i = 0; i < s.size(); i++)
+		{
+			for (int sz = 1; sz <= s.size(); sz++)
+			{
+				string sub = s.substr(i, sz);
+				if (isPalindrome(sub) && ret.size() < sub.size())
+				{
+					ret = sub;
+				}
+			}
+		}
+
 		return ret;
+	}
+};
+
+
+
+class Solution {
+public:
+	string longestPalindrome(string s) {
+		int n = s.size();
+		vector<vector<bool>> dp(n, vector<bool>(n));
+		array<int, 2> ans = { 0, 0 };
+
+		for (int i = 0; i < n; ++i) {
+			dp[i][i] = true;
+		}
+
+		for (int i = 0; i < n - 1; ++i) {
+			if (s[i] == s[i + 1]) {
+				dp[i][i + 1] = true;
+				ans = { i, i + 1 };
+			}
+		}
+
+		for (int diff = 2; diff < n; ++diff) {
+			for (int i = 0; i < n - diff; ++i) {
+				int j = i + diff;
+				if (s[i] == s[j] && dp[i + 1][j - 1]) {
+					dp[i][j] = true;
+					ans = { i, j };
+				}
+			}
+		}
+
+		int i = ans[0];
+		int j = ans[1];
+		return s.substr(i, j - i + 1);
 	}
 };
