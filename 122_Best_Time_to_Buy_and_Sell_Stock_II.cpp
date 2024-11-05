d16b4c38e893b683ff03bed8866f4767bc6ec9a9
ordinarydennis <ordinarydennis@gmail.com>
2024-11-05 19:38:29 +0900

    dp - 122. Best Time to Buy and Sell Stock II(solved again)

--- 122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ 122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -1,95 +1,3 @@
-class Solution {
-public:
-	int minimumTotal(vector<vector<int>>& triangle) {
-
-		int maxY = triangle.size();
-
-		vector<vector<int>> triangleSum(maxY);
-
-		triangleSum[0].push_back(triangle[0][0]);
-
-		for (int y = 1; y < maxY; y++)
-		{
-			int maxX = triangle[y].size();
-
-			for(int x = 0; x < maxX; x++)
-			{
-				if (0 == x)
-				{
-					triangleSum[y].push_back(triangleSum[y - 1][x] + triangle[y][x]);
-				}
-				else if (maxX - 1 == x)
-				{
-					triangleSum[y].push_back(triangleSum[y - 1][x - 1] + triangle[y][x]);
-				}
-				else
-				{
-					int m = std::min(triangleSum[y - 1][x - 1], triangleSum[y - 1][x]);
-
-					triangleSum[y].push_back(m + triangle[y][x]);
-				}
-			}
-		}
-
-		int ret = INT_MAX;
-
-		for (int x = 0; x < triangleSum[maxY - 1].size(); x++)
-		{
-			ret = std::min(ret, triangleSum[maxY - 1][x]);
-		}
-
-		return ret;
-	}
-};
-
-
-class Solution {
-public:
-	int minimumTotal(vector<vector<int>>& triangle) {
-
-		int maxY = triangle.size();
-
-		vector<vector<int>> totalSum(maxY);
-
-		totalSum[0].push_back(triangle[0][0]);
-
-		for (int y = 1; y < maxY; y++)
-		{
-			int maxX = triangle[y].size();
-
-			for (int x = 0; x < maxX; x++)
-			{
-				int n = 0;
-
-				if (0 == x)
-				{
-					n = totalSum[y - 1][x];
-				}
-				else if (maxX - 1 == x)
-				{
-					n = totalSum[y - 1][x - 1];
-				}
-				else
-				{
-					n = std::min(totalSum[y - 1][x], totalSum[y - 1][x - 1]);
-				}
-
-				totalSum[y].push_back(n + triangle[y][x]);
-			}
-		}
-
-		int ret = INT_MAX;
-
-
-		for(int x = 0; x < triangle[maxY - 1].size(); x++)
-		{
-			ret = std::min(totalSum[maxY - 1][x], ret);
-			
-		}
-
-		return ret;
-	}
-};
 
 class Solution {
 public:
@@ -111,9 +19,9 @@ private:
 		int max = 0;
 
 		for (int start = s; start < prices.size(); start++) {
-			
+
 			int maxprofit = 0;
-			
+
 			for (int i = start + 1; i < prices.size(); i++)
 			{
 				if (prices[start] < prices[i])
@@ -164,5 +72,40 @@ public:
 	int maxProfit(vector<int>& prices)
 	{
 		return dfs(0, prices);
+	}
+};
+
+
+class Solution {
+
+	int dfs(int buy, vector<int>& prices)
+	{
+		int ret = 0;
+
+		for (int b = buy; b < prices.size(); b++)
+		{
+			int max = 0;
+
+			for (int s = b + 1; s < prices.size(); s++)
+			{
+				if (prices[b] < prices[s])
+				{
+					int profit = dfs(s + 1, prices) + prices[s] - prices[b];
+					max = std::max(max, profit);
+				}
+			}
+
+			ret = std::max(max, ret);
+		}
+
+		return ret;
+	}
+
+
+public:
+	int maxProfit(vector<int>& prices) {
+
+		return dfs(0, prices);
+
 	}
 };
