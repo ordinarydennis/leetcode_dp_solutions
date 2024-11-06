90ac6af3cf0c1814c3733c2dfbda499e0f880220
ordinarydennis <ordinarydennis@gmail.com>
2024-11-06 21:35:06 +0900

    DP - 122. Best Time to Buy and Sell Stock II(read editorial, Approach 2: Peak Valley Approach, Approach 3: Simple One Pass)

--- 122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ 122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -1,3 +1,95 @@
+class Solution {
+public:
+	int minimumTotal(vector<vector<int>>& triangle) {
+
+		int maxY = triangle.size();
+
+		vector<vector<int>> triangleSum(maxY);
+
+		triangleSum[0].push_back(triangle[0][0]);
+
+		for (int y = 1; y < maxY; y++)
+		{
+			int maxX = triangle[y].size();
+
+			for(int x = 0; x < maxX; x++)
+			{
+				if (0 == x)
+				{
+					triangleSum[y].push_back(triangleSum[y - 1][x] + triangle[y][x]);
+				}
+				else if (maxX - 1 == x)
+				{
+					triangleSum[y].push_back(triangleSum[y - 1][x - 1] + triangle[y][x]);
+				}
+				else
+				{
+					int m = std::min(triangleSum[y - 1][x - 1], triangleSum[y - 1][x]);
+
+					triangleSum[y].push_back(m + triangle[y][x]);
+				}
+			}
+		}
+
+		int ret = INT_MAX;
+
+		for (int x = 0; x < triangleSum[maxY - 1].size(); x++)
+		{
+			ret = std::min(ret, triangleSum[maxY - 1][x]);
+		}
+
+		return ret;
+	}
+};
+
+
+class Solution {
+public:
+	int minimumTotal(vector<vector<int>>& triangle) {
+
+		int maxY = triangle.size();
+
+		vector<vector<int>> totalSum(maxY);
+
+		totalSum[0].push_back(triangle[0][0]);
+
+		for (int y = 1; y < maxY; y++)
+		{
+			int maxX = triangle[y].size();
+
+			for (int x = 0; x < maxX; x++)
+			{
+				int n = 0;
+
+				if (0 == x)
+				{
+					n = totalSum[y - 1][x];
+				}
+				else if (maxX - 1 == x)
+				{
+					n = totalSum[y - 1][x - 1];
+				}
+				else
+				{
+					n = std::min(totalSum[y - 1][x], totalSum[y - 1][x - 1]);
+				}
+
+				totalSum[y].push_back(n + triangle[y][x]);
+			}
+		}
+
+		int ret = INT_MAX;
+
+
+		for(int x = 0; x < triangle[maxY - 1].size(); x++)
+		{
+			ret = std::min(totalSum[maxY - 1][x], ret);
+			
+		}
+
+		return ret;
+	}
+};
 
 class Solution {
 public:
@@ -19,14 +111,13 @@ private:
 		int max = 0;
 
 		for (int start = s; start < prices.size(); start++) {
-
+			
 			int maxprofit = 0;
-
-			for (int i = start + 1; i < prices.size(); i++)
-			{
-				if (prices[start] < prices[i])
-				{
-					int profit = calculate(prices, i + 1) + prices[i] - prices[start];
+			
+			for (int i = start + 1; i < prices.size(); i++) {
+				if (prices[start] < prices[i]) {
+					int profit =
+						calculate(prices, i + 1) + prices[i] - prices[start];
 					if (profit > maxprofit) maxprofit = profit;
 				}
 			}
@@ -36,76 +127,61 @@ private:
 	}
 };
 
-
-
+//Approach 2: Peak Valley Approach
 class Solution {
-
-	int dfs(int s, vector<int>& prices)
-	{
-		if (prices.size() <= s)
-		{
-			return 0;
-		}
-
-		int max = 0;
-
-		for (int start = s; start < prices.size(); start++)
-		{
-			int maxprofit = 0;
-
-			for (int i = start + 1; i < prices.size(); i++)
-			{
-				if (prices[start] < prices[i])
-				{
-					int profit = dfs(i + 1, prices) + prices[i] - prices[start];
-					maxprofit = std::max(maxprofit, profit);
-				}
-			}
-
-			if (maxprofit > max) max = maxprofit;
-		}
-
-		return max;
-	}
-
-public:
-	int maxProfit(vector<int>& prices)
-	{
-		return dfs(0, prices);
-	}
-};
-
-
-class Solution {
-
-	int dfs(int buy, vector<int>& prices)
-	{
-		int ret = 0;
-
-		for (int b = buy; b < prices.size(); b++)
-		{
-			int max = 0;
-
-			for (int s = b + 1; s < prices.size(); s++)
-			{
-				if (prices[b] < prices[s])
-				{
-					int profit = dfs(s + 1, prices) + prices[s] - prices[b];
-					max = std::max(max, profit);
-				}
-			}
-
-			ret = std::max(max, ret);
-		}
-
-		return ret;
-	}
-
-
 public:
 	int maxProfit(vector<int>& prices) {
+		int i = 0;
+		int valley = prices[0];
+		int peak = prices[0];
+		int maxprofit = 0;
 
-		return dfs(0, prices);
+		while (i < prices.size() - 1)
+		{
+			while (i < prices.size() - 1 && prices[i] >= prices[i + 1]) i++;
 
+			valley = prices[i];
+
+			while (i < prices.size() - 1 && prices[i] <= prices[i + 1]) i++;
+
+			peak = prices[i];
+
+			maxprofit += peak - valley;
+		}
+		return maxprofit;
 	}
 };
+
+/*
+이 코드에서 valley를 먼저 구하고 peak을 구해야 하는 이유는 이익을 올리기 위해 주식을 저가에 매수하고 고가에 매도해야 하기 때문입니다. 
+만약 peak을 먼저 구하고 valley를 나중에 구한다면, 주식을 고가에 매수한 후 더 낮은 가격에 매도하는 상황이 발생할 수 있어 이익을 제대로 계산하지 못합니다
+*/
+
+//Approach 3: Simple One Pass
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		int maxprofit = 0;
+		for (int i = 1; i < prices.size(); i++) {
+			if (prices[i] > prices[i - 1])
+				maxprofit += prices[i] - prices[i - 1];
+		}
+		return maxprofit;
+	}
+};
+
+//also 
+
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		int maxprofit = 0;
+		for (int i = 0; i < prices.size() - 1; i++) {
+			if (prices[i] < prices[i + 1])
+				maxprofit += prices[i + 1] - prices[i];
+		}
+		return maxprofit;
+	}
+};
+
+//결국 낮은 가격에 사서 비싼 가격에 파는 로직을 짜야한다.
