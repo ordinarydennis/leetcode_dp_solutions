15a468d1ff5da619d1a132d0bf9ac8a61f071516
ordinarydennis <ordinarydennis@gmail.com>
2024-11-11 13:21:07 +0900

    DP - 131. Palindrome Partitioning(try again and using dp)

--- 131_Palindrome_Partitioning.cpp
+++ 131_Palindrome_Partitioning.cpp
@@ -56,3 +56,62 @@ public:
 		return ret;
 	}
 };
+
+
+class Solution {
+
+
+	void dfs(int index, string s, vector<string>& subStrList, vector<vector<bool>>& dp)
+	{
+		if (s.size() <= index)
+		{
+			ret.push_back(subStrList);
+			return;
+		}
+
+		for (int i = index; i < s.size(); i++)
+		{
+			if (index == i)
+			{
+				dp[index][i] = true;
+			}
+			else
+			{
+				if (s[index] == s[i])
+				{
+					if (i - index < 2 || dp[index + 1][i - 1])
+					{
+						dp[index][i] = true;
+					}
+				}
+			}
+
+			if (dp[index][i])
+			{
+			
+				subStrList.push_back(s.substr(index, i - index + 1));
+
+				dfs(i + 1, s, subStrList, dp);
+
+				subStrList.pop_back();
+			}
+
+		}
+
+	}
+
+	vector<vector<string>> ret;
+
+public:
+	vector<vector<string>> partition(string s) {
+
+
+		vector<vector<bool>> dp(s.size(), vector<bool>(s.size()));
+
+		vector<string> subStrList;
+
+		dfs(0, s, subStrList, dp);
+
+		return ret;
+	}
+};
