8b44b1ce70c815f971541ac5e511928b2e03389a
ordinarydennis <ordinarydennis@gmail.com>
2024-11-17 15:37:43 +0900

    DP - 131. Palindrome Partitioning(solved again as dp)

--- 131_Palindrome_Partitioning.cpp
+++ 131_Palindrome_Partitioning.cpp
@@ -115,3 +115,102 @@ public:
 		return ret;
 	}
 };
+
+
+//fail
+class Solution {
+
+	void dfs(int start, string& s, vector<string>& list, vector<vector<bool>>& dp)
+	{
+		if (s.size() <= start)
+		{
+			ret.push_back(list);
+			return;
+		}
+
+
+		for (int l = 1; l <= s.size(); l++)
+		{
+			if (1 == l)
+			{
+				dp[start][start] = true;
+			}
+			else if (2 == l && s[start] == s[start + 1])
+			{
+				dp[start][start + 1] = true;
+			}
+			else if (s[start] == s[start + l - 1] && dp[start + 1][start + l - 2])
+			{
+				dp[start][start + l - 1] = true;
+			}
+			else
+			{
+				continue;
+			}
+
+			list.emplace_back(std::move(s.substr(start, l)));
+
+			dfs(start + l, s, list, dp);
+
+			list.pop_back();
+		}
+	}
+
+	vector<vector<string>> ret;
+
+public:
+	vector<vector<string>> partition(string s) {
+
+		vector<vector<bool>> dp(s.size(), vector<bool>(s.size()));
+
+		vector<string> list;
+
+		dfs(0, s, list, dp);
+
+		return ret;
+	}
+
+};
+
+class Solution {
+
+	void dfs(int index, string& s, vector<string>& list, vector<vector<bool>>& dp)
+	{
+		if (s.size() <= index)
+		{
+			ret.push_back(list);
+			return;
+		}
+
+		for (int i = index; i < s.size(); i++)
+		{
+			if (s[index] == s[i])
+			{
+				if (i - index <= 2 || dp[index + 1][i - 1])
+				{
+					dp[index][i] = true;
+
+					list.emplace_back(s.substr(index, i - index + 1));
+
+					dfs(i + 1, s, list, dp);
+
+					list.pop_back();
+				}
+			}
+		}
+	}
+
+	vector<vector<string>> ret;
+
+public:
+	vector<vector<string>> partition(string s) {
+
+		vector<vector<bool>> dp(s.size(), vector<bool>(s.size()));
+
+		vector<string> list;
+
+		dfs(0, s, list, dp);
+
+		return ret;
+	}
+};
