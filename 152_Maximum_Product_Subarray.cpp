771271bb703d6aee4c97c2c367b323d8a33751e2
ordinarydennis <ordinarydennis@gmail.com>
2024-11-20 13:30:04 +0900

    DP - 152. Maximum Product Subarray(try again)

--- 152_Maximum_Product_Subarray.cpp
+++ 152_Maximum_Product_Subarray.cpp
@@ -69,3 +69,40 @@ public:
 	}
 };
 
+
+
+class Solution {
+
+	int dfs(int index, vector<int>& nums)
+	{
+		if (nums.size() - 1 == index)
+		{
+			result = nums[index];
+			return nums[index];
+		}
+		
+		int ret = dfs(index + 1, nums);
+
+		if (ret <= nums[index] * ret)
+		{
+			result = nums[index] * ret;
+			return nums[index] * ret;
+		}
+		else
+		{
+			return 1;
+		}
+
+		return 1;
+	}
+
+	int result = INT_MIN;
+
+public:
+	int maxProduct(vector<int>& nums) {
+
+		dfs(0, nums);
+
+		return result;
+	}
+};
