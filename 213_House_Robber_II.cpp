02f9dd7b6268e2cc3b160d76291ccf650b32816d
ordinarydennis <ordinarydennis@gmail.com>
2024-11-26 23:20:22 +0900

    DP - 213. House Robber II(read editorial)

--- 213_House_Robber_II.cpp
+++ 213_House_Robber_II.cpp
@@ -39,3 +39,29 @@ public:
 
 	}
 };
+
+class Solution {
+public:
+	int rob(vector<int>& nums) {
+		if (nums.size() == 0) return 0;
+
+		if (nums.size() == 1) return nums[0];
+
+		int max1 = rob_simple(nums, 0, nums.size() - 2);
+		int max2 = rob_simple(nums, 1, nums.size() - 1);
+
+		return max(max1, max2);
+	}
+
+private:
+	int rob_simple(vector<int>& nums, int start, int end) {
+		int t1 = 0, t2 = 0;
+		for (int i = start; i <= end; ++i) {
+			int temp = t1;
+			t1 = max(nums[i] + t2, t1);
+			t2 = temp;
+		}
+
+		return t1;
+	}
+};
