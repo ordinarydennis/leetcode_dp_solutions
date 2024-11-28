3afb5adfcebb40c49b9531954df06fd2b1d86741
ordinarydennis <ordinarydennis@gmail.com>
2024-11-28 13:36:02 +0900

    DP - 221. Maximal Square(read editorial, dp)

--- 221_Maximal_Square.cpp
+++ 221_Maximal_Square.cpp
@@ -59,3 +59,32 @@ public:
 		return maxLen * maxLen;
 	}
 };
+
+//dp
+class Solution {
+public:
+	int maximalSquare(vector<vector<char>>& matrix) {
+		int rows = matrix.size(), cols = rows > 0 ? matrix[0].size() : 0;
+		vector<vector<int>> dp(rows + 1, vector<int>(cols + 1, 0));
+		int maxsqlen = 0;
+		// for convenience, we add an extra all zero column and row
+		// outside of the actual dp table, to simpify the transition
+		for (int i = 1; i <= rows; i++)
+		{
+			for (int j = 1; j <= cols; j++)
+			{
+				if (matrix[i - 1][j - 1] == '1') 
+				{
+					dp[i][j] =
+						min(
+							min(dp[i][j - 1], dp[i - 1][j]), 
+							dp[i - 1][j - 1]
+						) +
+						1;
+					maxsqlen = max(maxsqlen, dp[i][j]);
+				}
+			}
+		}
+		return maxsqlen * maxsqlen;
+	}
+};
