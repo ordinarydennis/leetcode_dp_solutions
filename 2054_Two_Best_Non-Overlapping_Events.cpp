da28a4673c7ed408a442c92628e1bef4fb3a7577
ordinarydennis <ordinarydennis@gmail.com>
2025-02-22 23:36:55 +0900

    DP - 2054. Two Best Non-Overlapping Events(read editorial, try again)

--- 2054_Two_Best_Non-Overlapping_Events.cpp
+++ 2054_Two_Best_Non-Overlapping_Events.cpp
@@ -65,29 +65,61 @@ public:
 	}
 };
 
+class Solution {
+public:
+	int maxTwoEvents(vector<vector<int>>& events) {
+		vector<array<int, 3>> times;
+		for (auto& e : events) {
+			// 1 denotes start time.
+			times.push_back({ e[0], 1, e[2] });
+			// 0 denotes end time.
+			times.push_back({ e[1] + 1, 0, e[2] });
+		}
+		int ans = 0, maxValue = 0;
+		sort(begin(times), end(times));
+		for (auto& timeValue : times) {
+			// If current time is a start time, find maximum sum of maximum end
+			// time till now.
+			if (timeValue[1]) {
+				ans = max(ans, timeValue[2] + maxValue);
+			}
+			else {
+				maxValue = max(maxValue, timeValue[2]);
+			}
+		}
+		return ans;
+	}
+};
+
 
 class Solution {
 public:
 	int maxTwoEvents(vector<vector<int>>& events) {
-		// Create a min-heap to store the ending time with value.
-		priority_queue<pair<int, int>, vector<pair<int, int>>,
-			greater<pair<int, int>>>
-			pq;
-		sort(events.begin(), events.end());
 
-		int maxVal = 0, maxSum = 0;
+		std::priority_queue < std::pair<int, int>,
+			vector<std::pair<int, int>>,
+			std::greater<std::pair<int, int>>
+		> pq;
 
-		for (auto& event : events) {
-			// Pop all valid events from queue and take their maximum.
-			while (pq.size() && pq.top().first < event[0]) {
-				maxVal = max(maxVal, pq.top().second);
+
+		std::sort(events.begin(), events.end());
+
+		int ret = INT_MIN;
+		int max = 0;
+
+		for (auto& e : events)
+		{
+			while (pq.size() && pq.top().first < e[0])
+			{
+				max = std::max(max, pq.top().second);
 				pq.pop();
 			}
 
-			maxSum = max(maxSum, maxVal + event[2]);
-			pq.push({ event[1], event[2] });
+			pq.push({ e[1], e[2] });
+
+			ret = std::max(ret, max + e[2]);
 		}
 
-		return maxSum;
+		return ret;
 	}
 };
